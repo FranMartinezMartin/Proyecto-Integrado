089db3e735a50b5521c6be0683713673c185afdb
Francisco Martínez Martín <francismartinez2000@gmail.com>
2020-05-21 15:30:03 +0200

    Calendar menu

---
+++ app/src/main/java/org/dipalme/proteApp/data/CalendarAvailabilityState.kt
@@ -0,0 +1,12 @@
+package org.dipalme.proteApp.data
+
+data class CalendarAvailabilityState (
+    val available_00_06: Boolean? = null,
+    val available_06_12: Boolean? = null,
+    val available_12_18: Boolean? = null,
+    val available_18_24: Boolean? = null,
+    val available_00_12: Boolean? = null,
+    val available_06_18: Boolean? = null,
+    val available_12_24: Boolean? = null,
+    val available_00_24: Boolean? = null
+)
